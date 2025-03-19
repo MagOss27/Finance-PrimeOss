2ce55115aca0e0fb591f8c9615fcf77b85678a68
MagOss27 <MagnusOss98@gmail.com>
2025-03-18 22:06:04 -0300

    fix: criação do banco de dados utilizando Drizzle

--- components/welcome-msg.tsx
+++ components/welcome-msg.tsx
@@ -2,17 +2,18 @@
 
 import { useUser } from "@clerk/nextjs";
 
-export const WelcomeMsg = ()=> {
-    const {user, isLoaded} = useUser()
+export const WelcomeMsg = () => {
+  const { user, isLoaded } = useUser();
 
-    return (
-        <div className="space-y-2 mb-4">
-            <h2 className="text-2xl lg:text-4xl text-white">
-                Welcome Back
-            </h2>
-            <p>
-                This is your Financial Overview Report
-            </p>
-        </div>
-    )
-}
+  return (
+    <div className="space-y-2 mb-4">
+      <h2 className="text-2xl lg:text-4xl text-white font-medium">
+        Welcome Back{isLoaded ? ", " : ""}
+        {user?.firstName} 👋🏻
+      </h2>
+      <p className="text-sm lg:text-base text-[#89b6fd]">
+        This is your Financial Overview Report
+      </p>
+    </div>
+  );
+};
