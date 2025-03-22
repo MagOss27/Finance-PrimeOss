e0e2420b74583c645807ab55e90308fcffb402be
MagOss27 <MagnusOss98@gmail.com>
2025-03-22 19:58:49 -0300

    fix: finalização do account-form e new account sheet

--- features/accounts/components/account-form.tsx
+++ features/accounts/components/account-form.tsx
@@ -38,18 +38,56 @@ export const AccountForm = ({
 }: Props) => {
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
-    defaultValues,
+    defaultValues: defaultValues,
   });
 
   const handleSubmit = (values: FormValues) => {
-    console.log({ values });
+    onSubmit(values);
   };
 
-  return ( 
-  
-  <Form>
-    
-  </Form>
+  const handleDelete = () => {
+    onDelete?.();
+  };
 
-  )
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="space-y-4 pt-4 px-4"
+      >
+        <FormField
+          name="name"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input
+                  disabled={disabled}
+                  placeholder="e.g. Cash, Bank, Credit Card"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button className="w-full " disabled={disabled}>
+          {id ? "Save changes" : "Create account"}
+        </Button>
+        {!!id && (
+          <Button
+            type="button"
+            disabled={disabled}
+            onClick={handleDelete}
+            className="w-full "
+            size="icon"
+            variant="outline"
+          >
+            <Trash className="size-4 mr-2" />
+            Delete account
+          </Button>
+        )}
+      </form>
+    </Form>
+  );
 };
