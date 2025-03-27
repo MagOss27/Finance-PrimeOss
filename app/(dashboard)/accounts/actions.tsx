281f637cf26d2dfd2f12dda84e90aa37e1eddda4
MagOss27 <MagnusOss98@gmail.com>
2025-03-26 23:56:02 -0300

    fix: criação do actions com editar user

--- app/(dashboard)/accounts/actions.tsx
+++ app/(dashboard)/accounts/actions.tsx
@@ -1,13 +1,34 @@
-"use client"
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenuContent,
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { Edit, MoreHorizontal } from "lucide-react";
 
 type Props = {
-    id: string
-}
+  id: string;
+};
 
-export const Actions = ({id}: Props)=>{
-    return(
-        <div>
-            Actions
-        </div>
-    )
-}
+export const Actions = ({ id }: Props) => {
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="size-8 p-0">
+            <MoreHorizontal />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem disabled={false} onClick={() => {}}>
+            <Edit className="size-4 mr-2" />
+            Edit
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
